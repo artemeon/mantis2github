ec94180f48bdb535b470ab7626b7584f2442c19b
Marc Reichel <marc.reichel@artemeon.de>
2025-03-24 19:34:57 +0100

    chore: Reorder issue title parts

--- src/Dto/GithubIssue.php
+++ src/Dto/GithubIssue.php
@@ -34,7 +34,7 @@ class GithubIssue
         $issueBadge = '[![MANTIS-' . $issue->getId() . '](https://img.shields.io/badge/MANTIS-' . $issue->getId() . '-green?style=for-the-badge)](' . $issue->getIssueUrl() . ')';
 
         return new self(
-            title: '[MANTIS-' . $issue->getId() . '] [' . $issue->getProject() . '] ' . $issue->getSummary(),
+            title: $issue->getSummary() . '[' . $issue->getProject() . '] [MANTIS-' . $issue->getId() . ']',
             description: $issue->getDescription() . PHP_EOL . PHP_EOL . $issueBadge,
         );
     }
